8f073a145406c048da743ccdbd57b8432ef134c2
jmcb <joelsgp@protonmail.com>
2024-01-10 21:35:26 +0000

    Update train.py

--- train.py
+++ train.py
@@ -3,12 +3,20 @@ from transformers import GPT2Model, Trainer, TrainingArguments
 
 from common import DATASET_DIR, MODEL_NAME
 
+# https://huggingface.co/docs/transformers/v4.36.1/en/training
+# https://huggingface.co/docs/transformers/v4.36.1/en/main_classes/trainer#transformers.TrainingArguments
+# https://huggingface.co/gpt2
+# https://github.com/huggingface/transformers/blob/main/examples/pytorch/language-modeling/README.md
+
 OUTPUT_DIR = "eggyboi_model/"
 
 model = GPT2Model.from_pretrained(MODEL_NAME)
+
 dataset = load_dataset(DATASET_DIR)
+dataset_train = dataset["train"][0]
+
 training_args = TrainingArguments(output_dir=OUTPUT_DIR)
-trainer = Trainer(model=model, train_dataset=dataset, args=training_args)
+trainer = Trainer(model=model, train_dataset=dataset_train, args=training_args)
 
 
 def main():
