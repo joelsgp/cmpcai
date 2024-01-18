62f2fd0126f0fe23f46d8bbe8c1b5f95e012bce4
jmcb <joelsgp@protonmail.com>
2024-01-18 12:26:52 +0000

    Truncation, more notes

--- tokenise.py
+++ tokenise.py
@@ -3,13 +3,15 @@ from transformers import GPT2Tokenizer
 
 from common import DATA_TEXT_PATH, DATASET_DIR, MODEL_NAME
 
+# https://huggingface.co/docs/transformers/v4.36.1/en/main_classes/tokenizer#transformers.PreTrainedTokenizer.__call__
+# https://huggingface.co/docs/datasets/v2.16.1/en/package_reference/main_classes#datasets.Dataset.map
 
 def main():
     dataset = load_dataset("text", data_files={"train": DATA_TEXT_PATH})
     tokeniser = GPT2Tokenizer.from_pretrained(MODEL_NAME)
 
     def tokenise_function(examples):
-        return tokeniser(examples["text"])
+        return tokeniser(examples["text"], truncation=True)
 
     encoded = dataset.map(tokenise_function)
     encoded.save_to_disk(DATASET_DIR)
--- train.py
+++ train.py
@@ -3,9 +3,17 @@ from transformers import GPT2Model, Trainer, TrainingArguments
 
 from common import DATASET_DIR, MODEL_NAME
 
+# training / fine-tuning tutorial
 # https://huggingface.co/docs/transformers/v4.36.1/en/training
+
+# TrainingArguments
 # https://huggingface.co/docs/transformers/v4.36.1/en/main_classes/trainer#transformers.TrainingArguments
+
+# GPT2 docs
 # https://huggingface.co/gpt2
+# https://huggingface.co/docs/transformers/v4.36.1/en/model_doc/gpt2
+
+# train example scripts
 # https://github.com/huggingface/transformers/blob/main/examples/pytorch/language-modeling/README.md
 
 OUTPUT_DIR = "eggyboi_model/"
@@ -15,7 +23,7 @@ def main():
     model = GPT2Model.from_pretrained(MODEL_NAME)
 
     dataset = load_dataset(DATASET_DIR)
-    dataset_train = dataset["train"][0]
+    dataset_train = dataset["train"]
 
     training_args = TrainingArguments(output_dir=OUTPUT_DIR)
     trainer = Trainer(model=model, train_dataset=dataset_train, args=training_args)
